52b7fad719759f459ff704cb41d344ca46ee9213
Bryan Crotaz <bgcrotaz@hotmail.com>
2021-11-27 14:55:40 +0000

    clean up decorator

--- addon/classes/tracked-effect-decorator.ts
+++ addon/classes/tracked-effect-decorator.ts
@@ -1,19 +1,19 @@
-//import TrackedEffectsService from "./tracked-effects-core";
-
 import { assert } from "@ember/debug";
 import TrackedEffectsCore from "./tracked-effects-core";
 
-// @ts-ignore
-// eslint-disable-next-line
-export default function effect(this: any, target: any, propertyKey: string, descriptor?: PropertyDescriptor = undefined): any {
+export default function effect(this: any, target: any, propertyKey: string, descriptor?: {initializer?: Function}): any {
   // @ts-ignore
-  var oldInit = descriptor.initializer;
+  var oldInit = descriptor?.initializer;
 
   assert(`You can only use the @effect decorator on the ${target.constructor.name} class on a property and not on a method. Rewrite '@effect ${propertyKey}() {}' as '@effect ${propertyKey} = () => {};'`, oldInit)
-  // @ts-ignore
-  descriptor.initializer = function() {
-    var fn = oldInit.call(this);
-    TrackedEffectsCore.instance?.addEffect(fn, this);    
-    return fn;
-  };
+
+  if (descriptor && oldInit) {
+    descriptor.initializer = function() {
+      var fn = oldInit?.call(this);
+      if (fn) {
+        TrackedEffectsCore.instance?.addEffect(fn, this);    
+      }
+      return fn;
+    };
+  }
 }
