7988ddfcad237e34de9363bd80d12eebacbeed0f
Bryan Crotaz <bgcrotaz@hotmail.com>
2021-11-29 19:29:44 +0000

    add exports for decorator

---
+++ addon/index.js
@@ -0,0 +1,7 @@
+import effect from './classes/tracked-effect-decorator';
+import TrackedEffect from './classes/tracked-effect';
+
+export {
+  effect,
+  TrackedEffect
+}
--- app/services/tracked-effects.js
+++ app/services/tracked-effects.js
@@ -1,5 +1,1 @@
 export { default } from 'ember-tracked-effects-placeholder/services/tracked-effects';
-import effect from 'ember-tracked-effects-placeholder/classes/tracked-effect-decorator';
-import TrackedEffect from 'ember-tracked-effects-placeholder/classes/tracked-effect';
-
-export { effect, TrackedEffect }
