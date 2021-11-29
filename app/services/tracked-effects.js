6578169b726e8f7d521ef7270590b1ff7e5ca192
Bryan Crotaz <bgcrotaz@hotmail.com>
2021-11-29 18:43:15 +0000

    add exports

--- app/services/tracked-effects.js
+++ app/services/tracked-effects.js
@@ -1,1 +1,3 @@
 export { default } from 'ember-tracked-effects-placeholder/services/tracked-effects';
+export { default as effect } from 'ember-tracked-effects-placeholder/classes/tracked-effect-decorator';
+export { TrackedEffect } from 'ember-tracked-effects-placeholder/classes/tracked-effect';
