19428a6f39f834c81def53410e3a4572a1d82af0
Bryan Crotaz <bgcrotaz@hotmail.com>
2022-02-22 11:43:00 +0000

    run the effect on the actions queue

--- addon/classes/tracked-effect.ts
+++ addon/classes/tracked-effect.ts
@@ -2,16 +2,18 @@ import { guidFor } from '@ember/object/internals';
 import { cached } from 'tracked-toolbox';
 import { registerDestructor } from '@ember/destroyable';
 import TrackedEffectsCore from './tracked-effects-core';
+import { schedule } from '@ember/runloop';
 
+export type TrackedEffectCallback = (...args: any[]) => any;
 export default class TrackedEffect {
   private _id: string;
-  private runFn: Function;
+  private runFn: TrackedEffectCallback;
 
   get id(): string {
     return this._id;
   }
 
-  constructor({ runFn, context }: { runFn: Function; context?: object; }) {
+  constructor({ runFn, context }: { runFn: TrackedEffectCallback; context?: object; }) {
     this.runFn = runFn;
     if (context) {
       registerDestructor(context, () => {
@@ -28,7 +30,7 @@ export default class TrackedEffect {
   @cached get run(): boolean {
     // cached only runs this getter if the inputs have changed
     // result is irrelevant
-    this.runFn();
+    schedule('actions', this.runFn);
     return true;
   }
 }
